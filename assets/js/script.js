a6be940ecb81a0bc618b562e508d117cb8130861
penaone <penaone@gmail.com>
2020-08-18 18:16:15 -0700

    Added Event Listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,22 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    
+
+    event.preventDefault();
+
+
     var listItemEl = document.createElement("li");
     listItemEl.classNme = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
+};
 
-buttonEl.addEventListener("click", function() {
+formEl.addEventListener("submit", createTaskHandler);
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
-});
+
 
